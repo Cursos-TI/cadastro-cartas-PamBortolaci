108b2da3a25ad5548b7e0f78ea7bc9deb8b1efd1
Pamela Ariane Bortolaci <pamkelka@gmail.com>
2025-09-17 17:43:13 +0000

    Alteração para cálculo populacional e PIB percapta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,20 +11,28 @@ int main() {
     char estado1[15] = "batata";
     char codigodacarta1[10] = "batata";
     char nomedacidade1[15] = "batata";
-    int populacao1 = 300.000;
+    int populacao1 = 300000;
     float areaemkm1 = 1.000000;
     float pib1 = 700.000000000;
     int pontosturisticos1 = 18;
+    float densidadepopulacional1 = 0.0;
+    float pibpercapta1 = 0.0;
+    float densidadepopulacional1 = 0.0;
+    float pibpercapta1 = 0.0;
   
   // Variáveis da segunda carta
 
     char estado2[15] = "batata";
     char codigodacarta2[10] = "batata";
     char nomedacidade2[15] = "batata";
-    int populacao2 = 300.000;
+    int populacao2 = 300000;
     float areaemkm2 = 1.000000;
     float pib2 = 700.000000000;
     int pontosturisticos2 = 18;
+    float densidadepopulacional2 = 0.0;
+    float pibpercapta2 = 0.0;
+    float densidadepopulacional2 = 0.0;
+    float pibpercapta2 = 0.0;
 
   // Área para entrada de dados da primeira carta
   
@@ -64,6 +72,14 @@ int main() {
 
     printf ("\n* Pontos turísticos..: %d\n", pontosturisticos1);
 
+    //Área para cálculo populacional e PIB per capta da primeira carta
+
+    densidadepopulacional1 = (float)populacao1 / areaemkm1;
+    pibpercapta1 = (float)pib1 / populacao1;
+
+    printf ("\n A densidade populacional de %s é de: %.2f", nomedacidade1, densidadepopulacional1);
+    printf ("\n O PIB per capta de %s é de: %.2f\n", nomedacidade1, pibpercapta1);
+
     // Área para entrada de dados da segunda carta
   
     printf ("\nCadastre sua carta:\n");
@@ -101,5 +117,14 @@ int main() {
     printf ("\n* PIB................: %.2f", pib2);
 
     printf ("\n* Pontos turísticos..: %d\n", pontosturisticos2);
+
+    //Área para cálculo populacional e PIB per capta da sefunda carta
+
+    densidadepopulacional2 = (float)populacao2 / areaemkm2;
+    pibpercapta2 = (float)pib2 / populacao2;
+
+    printf ("\n A densidade populacional de %s é de: %.2f", nomedacidade2, densidadepopulacional2);
+    printf ("\n O PIB per capta de %s é de: %.2f", nomedacidade2, pibpercapta2);
+
 return 0;
 } 
